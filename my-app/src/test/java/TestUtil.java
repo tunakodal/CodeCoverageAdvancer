0f6553fb88186083d626741ac1051af710ecc284
tunak <tunakodal43@gmail.com>
2024-03-27 01:49:31 +0300

    Additional tests has added to TestUtil.java, total code coverage has increased to %100

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,9 +1,7 @@
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TestUtil {
   Util c;
@@ -13,4 +11,37 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void lengthEqualsOne() {
+    boolean result = c.compute(1);
+    assertFalse(result);
+  }
+
+  @Test
+  public void lengthMod() {
+    boolean result = c.compute(1,2,3,4,5,6);
+    assertFalse(result);
+  }
+
+  @Test
+  public void throwsException() {
+    assertThrows(RuntimeException.class, () -> {
+      c.compute(0,2,3);
+    });
+  }
+
+  @Test
+  public void sumMod() {
+    boolean result = c.compute(1,2,3);
+    assertTrue(result);
+  }
+
+  @Test
+  public void sumModFalse() {
+    boolean result = c.compute(2,3,6);
+    assertFalse(result);
+  }
+
+
 }
